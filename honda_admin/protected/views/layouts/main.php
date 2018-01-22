ac2ac838fed0c70563224906346e9676107b2ecb
Ashish Chauhan <ashchauhan77@gmail.com>
2018-01-22 12:57:39 +0530

    alert on delete and generate certificate

--- honda_admin/protected/views/layouts/main.php
+++ honda_admin/protected/views/layouts/main.php
@@ -1,126 +1,116 @@
 <?php /* @var $this Controller */ ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="language" content="en">
-
-	<!-- Bootstrap core CSS-->
-	<link href="<?php echo Yii::app()->request->baseUrl; ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<!-- Custom fonts for this template-->
-	<link href="<?php echo Yii::app()->request->baseUrl; ?>/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	<!-- Page level plugin CSS-->
-	<link href="<?php echo Yii::app()->request->baseUrl; ?>/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
-	<!-- Custom styles for this template-->
-	<link href="<?php echo Yii::app()->request->baseUrl; ?>/css/sb-admin.css" rel="stylesheet">
-
-  <!-- Bootstrap core JavaScript-->
-  <script src="<?php echo Yii::app()->request->baseUrl; ?>/vendor/jquery/jquery.min.js"></script>
-  <script src="<?php echo Yii::app()->request->baseUrl; ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- Core plugin JavaScript-->
-  <script src="<?php echo Yii::app()->request->baseUrl; ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
-  <!-- Page level plugin JavaScript-->
-  <script src="<?php echo Yii::app()->request->baseUrl; ?>/vendor/datatables/jquery.dataTables.js"></script>
-  <script src="<?php echo Yii::app()->request->baseUrl; ?>/vendor/datatables/dataTables.bootstrap4.js"></script>
-  <!-- Custom scripts for all pages-->
-  <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/sb-admin.min.js"></script>
-  <!-- Custom scripts for this page-->
-  <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/sb-admin-datatables.min.js"></script>
-
-  <style type="text/css">
-    .errorMessage {
-      color:red;
-    }
-  </style>
-
-
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
-</head>	
-
-<body class="fixed-nav sticky-footer bg-dark" id="page-top">
-  <!-- Navigation-->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    <a class="navbar-brand" href="/"><?php echo CHtml::encode(Yii::app()->name); ?></a>
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
-       
-      
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
-          <a class="nav-link" href="<?= $this->createUrl('site/index') ?>">
-            <i class="fa fa-fw fa-table"></i>
-            <span class="nav-link-text">Certificate</span>
-          </a>
-        </li>
-       
-        
-      </ul>
-      <ul class="navbar-nav sidenav-toggler">
-        <li class="nav-item">
-          <a class="nav-link text-center" id="sidenavToggler">
-            <i class="fa fa-fw fa-angle-left"></i>
-          </a>
-        </li>
-      </ul>
-      <ul class="navbar-nav ml-auto">
-       
-        <li class="nav-item">
-          <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
-            <i class="fa fa-fw fa-sign-out"></i>Logout</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-
-	<?php echo $content; ?>
-
-	<!-- /.content-wrapper-->
-    <footer class="sticky-footer">
-      <div class="container">
-        <div class="text-center">
-          <small>Copyright © Your Website 2017</small>
-        </div>
-      </div>
-    </footer>
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-      <i class="fa fa-angle-up"></i>
-    </a>
-    <!-- Logout Modal-->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">×</span>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="language" content="en">
+        <!-- Bootstrap core CSS-->
+        <link href="<?php echo Yii::app()->request->baseUrl; ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Custom fonts for this template-->
+        <link href="<?php echo Yii::app()->request->baseUrl; ?>/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <!-- Page level plugin CSS-->
+        <link href="<?php echo Yii::app()->request->baseUrl; ?>/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+        <!-- Custom styles for this template-->
+        <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/sb-admin.css" rel="stylesheet">
+        <!-- Bootstrap core JavaScript-->
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/vendor/jquery/jquery.min.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- Core plugin JavaScript-->
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
+        <!-- Page level plugin JavaScript-->
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/vendor/datatables/jquery.dataTables.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/vendor/datatables/dataTables.bootstrap4.js"></script>
+        <!-- Custom scripts for all pages-->
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/sb-admin.min.js"></script>
+        <!-- Custom scripts for this page-->
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/sb-admin-datatables.min.js"></script>
+        <style type="text/css">
+            .errorMessage {
+            color:red;
+            }
+        </style>
+        <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    </head>
+    <body class="fixed-nav sticky-footer bg-dark" id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <a class="navbar-brand" href="/"><?php echo CHtml::encode(Yii::app()->name); ?></a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
             </button>
-          </div>
-          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-          <div class="modal-footer">
-            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-            <a class="btn btn-primary" href="<?= $this->createUrl('site/login') ?>">Logout</a>
-          </div>
-        </div>
-      </div>
-    </div>
-<script>
-    $(document).ready(function(){
-        // Add minus icon for collapse element which is open by default
-        $(".collapse.in").each(function(){
-        	$(this).siblings(".panel-heading").find(".fa").addClass("fa-minus").removeClass("fa-plus");
-        });
-        
-        // Toggle plus minus icon on show hide of collapse element
-        $(".collapse").on('show.bs.collapse', function(){
-        	$(this).parent().find(".fa").removeClass("fa-plus").addClass("fa-minus");
-        }).on('hide.bs.collapse', function(){
-        	$(this).parent().find(".fa").removeClass("fa-minus").addClass("fa-plus");
-        });
-    });
-</script>
-</body>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+                    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
+                        <a class="nav-link" href="<?= $this->createUrl('site/index') ?>">
+                        <i class="fa fa-fw fa-table"></i>
+                        <span class="nav-link-text">Certificate</span>
+                        </a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav sidenav-toggler">
+                    <li class="nav-item">
+                        <a class="nav-link text-center" id="sidenavToggler">
+                        <i class="fa fa-fw fa-angle-left"></i>
+                        </a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
+                        <i class="fa fa-fw fa-sign-out"></i>Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
+        <!-- view container -->
+        <?php echo $content; ?>
+        <!-- /.content-wrapper-->
+
+
+        <footer class="sticky-footer">
+            <div class="container">
+                <div class="text-center">
+                    <small>Copyright © Your Website 2017</small>
+                </div>
+            </div>
+        </footer>
+        <!-- Scroll to Top Button-->
+        <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fa fa-angle-up"></i>
+        </a>
+        <!-- Logout Modal-->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                    <div class="modal-footer">
+                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                        <a class="btn btn-primary" href="<?= $this->createUrl('site/login') ?>">Logout</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script>
+            $(document).ready(function(){
+                // Add minus icon for collapse element which is open by default
+                $(".collapse.in").each(function(){
+                  $(this).siblings(".panel-heading").find(".fa").addClass("fa-minus").removeClass("fa-plus");
+                });
+                
+                // Toggle plus minus icon on show hide of collapse element
+                $(".collapse").on('show.bs.collapse', function(){
+                  $(this).parent().find(".fa").removeClass("fa-plus").addClass("fa-minus");
+                }).on('hide.bs.collapse', function(){
+                  $(this).parent().find(".fa").removeClass("fa-minus").addClass("fa-plus");
+                });
+            });
+        </script>
+    </body>
 </html>
--- honda_admin/protected/views/site/index.php
+++ honda_admin/protected/views/site/index.php
@@ -1,146 +1,148 @@
 <div class="content-wrapper">
-<div class="container-fluid">
-   <?php if($status) { ?>
-
-      <div class="alert alert-<?= $status['code'] ?> alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-         <?= $status['message'] ?>
-     </div>
-
-   <?php }  ?>
-   <!-- Icon Cards-->
-   <div class="card mb-3">
-      <div class="card-header">
-         <i class="fa fa-certificate"></i> Create Certificate
-      </div>
-
-      <div class="card-body">
-         <div class="container">
-            <?php $form=$this->beginWidget('CActiveForm', array(
-               'id'=>'request-form',
-               'enableClientValidation'=>true,
-                  'htmlOptions' => array('enctype' => 'multipart/form-data'),
-               'clientOptions'=>array(
-                'validateOnSubmit'=>true,
-               ),
-               )); 
-                ?>
-            <div class="row">
-               <div class="col-md-6">
-                  <div class="form-group">
-                     <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-certificate"></i></span>
-                        <?php echo $form->textField($model,'name',array('class' => 'form-control',"placeholder"=>"Enter Certificate name ")); ?>                
-                     </div>
-                     <?php echo $form->error($model,'name'); ?>
-                  </div>
-                  <div class="form-group">
-                     <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-file"></i></span>
-                        <?php echo $form->fileField($model,'template_file',array('class' => 'form-control',"placeholder"=>"Enter Certificate Template")); ?>
-                     </div>
-                     <?php echo $form->error($model,'template_file'); ?>
-                  </div>
-               </div>
-               <div class="col-md-6">
-                  <div class="form-group">
-                     <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-certificate"></i></span>
-                        <?php echo $form->textarea($model,'description',array('class' => 'form-control',"placeholder"=>"Enter Certificate description",'rows' => 3)); ?>
-                     </div>
-                     <?php echo $form->error($model,'description'); ?>
-                  </div>
-               </div>
-               <div class="col-md-3">
-                  <button class="btn btn-primary btn-block" style="cursor: pointer;" type="submit">Create</button>
-               </div>
+    <div class="container-fluid">
+        <?php if($status) { ?>
+        <div class="alert alert-<?= $status['code'] ?> alert-dismissable">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+            <?= $status['message'] ?>
+        </div>
+        <?php }  ?>
+        <!-- Icon Cards-->
+        <div class="card mb-3">
+            <div class="card-header">
+                <i class="fa fa-certificate"></i> Create Certificate
             </div>
-            <?php $this->endWidget(); ?>
-         </div>         
-      </div>
-   </div>
-   
-   <!-- Icon Cards-->
-   <div class="card mb-3">
-      <div class="card-header">
-         <i class="fa fa-certificate"></i> Manage Certificate
-      </div>
-      <div class="card-body">
-         <div class="row">
-            <div class="col-md-6 form-group bg-default">
-               <button class="btn btn-primary" type="submit" data-toggle="collapse" data-parent="#accordion" href="#collapseOne"> <i class="fa fa-fw fa-plus"></i> Filters</button>
+            <div class="card-body">
+                <div class="container">
+                    <?php $form=$this->beginWidget('CActiveForm', array(
+                        'id'=>'request-form',
+                        'enableClientValidation'=>true,
+                           'htmlOptions' => array('enctype' => 'multipart/form-data'),
+                        'clientOptions'=>array(
+                         'validateOnSubmit'=>true,
+                        ),
+                        )); 
+                         ?>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="fa fa-certificate"></i></span>
+                                    <?php echo $form->textField($model,'name',array('class' => 'form-control',"placeholder"=>"Enter Certificate name ")); ?>                
+                                </div>
+                                <?php echo $form->error($model,'name'); ?>
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="fa fa-file"></i></span>
+                                    <?php echo $form->fileField($model,'template_file',array('class' => 'form-control',"placeholder"=>"Enter Certificate Template")); ?>
+                                </div>
+                                <?php echo $form->error($model,'template_file'); ?>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="fa fa-certificate"></i></span>
+                                    <?php echo $form->textarea($model,'description',array('class' => 'form-control',"placeholder"=>"Enter Certificate description",'rows' => 3)); ?>
+                                </div>
+                                <?php echo $form->error($model,'description'); ?>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <button class="btn btn-primary btn-block" style="cursor: pointer;" type="submit">Create</button>
+                        </div>
+                    </div>
+                    <?php $this->endWidget(); ?>
+                </div>
             </div>
-            <div id="collapseOne" class="panel-collapse collapse col-md-12 <?= isset($status['code']) &&  $status['code'] == 'info' ? 'show' : ''  ?> ">
-               <div class="panel-body">
-                  <?php $form=$this->beginWidget('CActiveForm', array(
-               'id'=>'request-form',
-               'enableClientValidation'=>true,
-               'clientOptions'=>array(
-                'validateOnSubmit'=>true,
-               ),
-               )); 
-                ?>
-                  <div class="form-group row">
-                     <div class="col-md-4">
-                        <input class="form-control"  type="text"  placeholder="Search By ID " name="filters[id]" value="<?= $filters['id'] ?>">
-                     </div>
-                     <div class="col-md-4">
-                        <input class="form-control" placeholder="Search By Certificate Name " name="filters[name]" value="<?= $filters['name'] ?>">
-                     </div>
-                     <div class="col-md-4">
-                        <input class="form-control" placeholder="Search By Certificate Name" name="filters[description]" value="<?= $filters['description'] ?>">
-                     </div>
-                  </div>
-                  <div class="form-group">
-                     <button class="btn btn-primary pull-right" style="margin-left: 10px" type="submit"> Search</button>
-                     <a class="btn btn-danger pull-right" href="<?= $this->createUrl('site/index') ?>">Clear Filter</a>
-                    
-                  </div>
-                <?php $this->endWidget(); ?>
-               </div>
+        </div>
+        <!-- Icon Cards-->
+        <div class="card mb-3">
+            <div class="card-header">
+                <i class="fa fa-certificate"></i> Manage Certificate
             </div>
-         </div>
-         <div class="row">
-            <div class="table-responsive">
-               <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                  <thead>
-                     <tr>
-                        <th>S.no</th>
-                        <th>ID</th>
-                        <th>Certificate Name</th>
-                        <th>Certificate Description</th>
-                        <th>Manage</th>
-                     </tr>
-                  </thead>
-                  <tbody>
-                     <?php $i=1;
-
-                     if(count($certificates) == 0) { ?>
-
-                     <td colspan="5">No record found</td>
-
-                     <?php 
-                     }
-                     foreach ($certificates as $key => $certificate) { ?>
-                     <tr>
-                        <td><?= $i++ ?></td>
-                        <td><?= $certificate->id ?></td>
-                        <td><?= $certificate->name ?></td>
-                        <td><?= $certificate->description ?></td>
-                        <td>
-                           <a href="<?= $this->createUrl('site/index/'.$certificate->id) ?>" class="btn btn-success"><i class="fa fa-fw fa-edit"></i> </a>
-                           <a class="btn btn-success" href="<?= $this->createUrl('site/configure/'.$certificate->id) ?>"><i class="fa fa-fw fa-cogs"></i></a>
-                           <a href="<?= $this->createUrl('site/delete/'.$certificate->id) ?>" class="btn btn-success"><i class="fa fa-fw fa-trash"></i></a>
-                        </td>
-                     </tr>
-                     <?php } ?>
-
-                  </tbody>
-               </table>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6 form-group bg-default">
+                        <button class="btn btn-primary" type="submit" data-toggle="collapse" data-parent="#accordion" href="#collapseOne"> <i class="fa fa-fw fa-plus"></i> Filters</button>
+                    </div>
+                    <div id="collapseOne" class="panel-collapse collapse col-md-12 <?= isset($status['code']) &&  $status['code'] == 'info' ? 'show' : ''  ?> ">
+                        <div class="panel-body">
+                            <?php $form=$this->beginWidget('CActiveForm', array(
+                                'id'=>'request-form',
+                                'enableClientValidation'=>true,
+                                'clientOptions'=>array(
+                                 'validateOnSubmit'=>true,
+                                ),
+                                )); 
+                                 ?>
+                            <div class="form-group row">
+                                <div class="col-md-4">
+                                    <input class="form-control"  type="text"  placeholder="Search By ID " name="filters[id]" value="<?= $filters['id'] ?>">
+                                </div>
+                                <div class="col-md-4">
+                                    <input class="form-control" placeholder="Search By Certificate Name " name="filters[name]" value="<?= $filters['name'] ?>">
+                                </div>
+                                <div class="col-md-4">
+                                    <input class="form-control" placeholder="Search By Certificate Name" name="filters[description]" value="<?= $filters['description'] ?>">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <button class="btn btn-primary pull-right" style="margin-left: 10px" type="submit"> Search</button>
+                                <a class="btn btn-danger pull-right" href="<?= $this->createUrl('site/index') ?>">Clear Filter</a>
+                            </div>
+                            <?php $this->endWidget(); ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="table-responsive">
+                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th>S.no</th>
+                                    <th>ID</th>
+                                    <th>Certificate Name</th>
+                                    <th>Certificate Description</th>
+                                    <th>Manage</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $i=1;
+                                    if(count($certificates) == 0) { ?>
+                                <td colspan="5">No record found</td>
+                                <?php 
+                                    }
+                                    foreach ($certificates as $key => $certificate) { ?>
+                                <tr>
+                                    <td><?= $i++ ?></td>
+                                    <td><?= $certificate->id ?></td>
+                                    <td><?= $certificate->name ?></td>
+                                    <td><?= $certificate->description ?></td>
+                                    <td>
+                                        <a href="<?= $this->createUrl('site/index/'.$certificate->id) ?>" class="btn btn-success"><i class="fa fa-fw fa-edit"></i> </a>
+                                        <a class="btn btn-success" href="<?= $this->createUrl('site/configure/'.$certificate->id) ?>"><i class="fa fa-fw fa-cogs"></i></a>
+                                        <a href="#" onclick="deleteCertificate(<?= $certificate->id  ?>)" class="btn btn-success"><i class="fa fa-fw fa-trash"></i></a>
+                                    </td>
+                                </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-   </div>
+        </div>
+    </div>
 </div>
 <!-- /.container-fluid-->
-
+<script type="text/javascript">
+    function deleteCertificate($id) {
+        
+        var response = confirm("Do you want to delete this certificate!");
+        if (response == true) {
+            window.location.href = "<?= $this->createUrl('site/delete/') ?>/"+$id;
+        } else {
+            console.log('cancel');
+        } 
+    }
+</script>
